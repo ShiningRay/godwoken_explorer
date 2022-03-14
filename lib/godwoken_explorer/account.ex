dd66f11617a8992ad3716473df323beaf7812d29
Miles Zhang <mingchang555@hotmail.com>
2022-03-14 12:42:51 +0800

    feat: add unknown transaction type
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer/account.ex
+++ lib/godwoken_explorer/account.ex
@@ -189,7 +189,7 @@ defmodule GodwokenExplorer.Account do
         if is_nil(udt) do
           %{
             sudt: %{
-              name: "Unkown##{id}"
+              name: "Unknown##{id}"
             }
           }
         else
@@ -204,7 +204,7 @@ defmodule GodwokenExplorer.Account do
 
           %{
             sudt: %{
-              name: udt.name || "Unkown##{id}",
+              name: udt.name || "Unknown##{id}",
               symbol: udt.symbol,
               icon: udt.icon,
               decimal: udt.decimal,
